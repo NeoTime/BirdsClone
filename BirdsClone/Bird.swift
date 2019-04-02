442246d37fe04249265cffd6894b55829bf6aedf
NeoTime <riglerjeff@gmail.com>
2019-04-02 15:51:45 -0700

    Pinch added

---
+++ BirdsClone/Bird.swift
@@ -0,0 +1,13 @@
+//
+//  Bird.swift
+//  BirdsClone
+//
+//  Created by Jefferey Rigler on 4/2/19.
+//  Copyright © 2019 Rigler Digital. All rights reserved.
+//
+
+import SpriteKit
+
+class Bird: SKSpriteNode {
+
+}
---
+++ BirdsClone/Configuration.swift
@@ -0,0 +1,25 @@
+//
+//  Configuration.swift
+//  BirdsClone
+//
+//  Created by Jefferey Rigler on 4/2/19.
+//  Copyright © 2019 Rigler Digital. All rights reserved.
+//
+
+import CoreGraphics
+
+extension CGPoint {
+    
+    static public func + (left: CGPoint, right: CGPoint) -> CGPoint {
+        return CGPoint(x: left.x + right.x, y: left.y + right.y)
+    }
+    
+    static public func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+    }
+    
+    static public func * (left: CGPoint, right: CGFloat) -> CGPoint {
+        return CGPoint(x: left.x * right, y: left.y * right)
+    }
+    
+}
