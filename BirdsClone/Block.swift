f33e0b2625324a6218dd0279fe0d2eeca22cdd2c
NeoTime <riglerjeff@gmail.com>
2019-04-03 10:26:27 -0700

    Animated Camera
    Animated Camera and add new bird after slingshot

---
+++ BirdsClone/Block.swift
@@ -0,0 +1,17 @@
+//
+//  Block.swift
+//  BirdsClone
+//
+//  Created by Jefferey Rigler on 4/3/19.
+//  Copyright © 2019 Rigler Digital. All rights reserved.
+//
+
+import SpriteKit
+
+enum BlockType: {
+    case wood, stone, glass
+}
+
+class Block: SKSpriteNode {
+
+}
