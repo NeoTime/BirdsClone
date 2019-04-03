119e85d947c1133b4a96be6ea610621c160004b9
NeoTime <riglerjeff@gmail.com>
2019-04-03 11:28:58 -0700

    Block Class Added
    For obstacles. Block class added

--- BirdsClone/Block.swift
+++ BirdsClone/Block.swift
@@ -8,10 +8,43 @@
 
 import SpriteKit
 
-enum BlockType: {
+enum BlockType: String {
     case wood, stone, glass
 }
 
 class Block: SKSpriteNode {
-
+    
+    let type: BlockType
+    var health: Int
+    let damageThreshold: Int
+    
+    init(type: BlockType) {
+        self.type = type
+        switch type {
+        case .wood:
+            health = 200
+        case .stone:
+            health = 500
+        case .glass:
+            health = 50
+            
+        }
+        damageThreshold = health/2
+        super.init(texture: nil, color: UIColor.clear, size: CGSize.zero)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func createPhysicsBody() {
+        physicsBody = SKPhysicsBody(rectangleOf: size)
+        physicsBody?.isDynamic = true
+        physicsBody?.categoryBitMask = PhysicsCategory.block
+        physicsBody?.contactTestBitMask = PhysicsCategory.all
+        physicsBody?.collisionBitMask = PhysicsCategory.all
+        
+        
+    }
+    
 }
