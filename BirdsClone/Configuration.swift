c1702b8ff09e0c35ba260c09cc972b256e044fa2
NeoTime <riglerjeff@gmail.com>
2019-04-05 14:07:46 -0700

    added menu and levels

--- BirdsClone/Configuration.swift
+++ BirdsClone/Configuration.swift
@@ -14,7 +14,7 @@ struct Levels {
 }
 
 struct ZPosition {
-    static let background: CGFloat = 0.0
+    static let background: CGFloat = 0
     static let obstacles: CGFloat = 1
     static let hudBackground: CGFloat = 10
     static let hudLabel: CGFloat = 11
